bb18c36b8641bd02c7912dea41f37c0a417b37eb
PrincePatel027 <princeghadiya699@gmail.com>
2024-12-09 17:22:02 +0530

    ScanQR - Android some error build.gradle

--- lib/screens/pages/scan_qr_code.dart
+++ lib/screens/pages/scan_qr_code.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:qr_plus/screens/components/my_button.dart';
 
 class ScanQrCode extends StatefulWidget {
   const ScanQrCode({super.key});
@@ -8,7 +10,7 @@ class ScanQrCode extends StatefulWidget {
 }
 
 class _ScanQrCodeState extends State<ScanQrCode> {
-  var urlController = TextEditingController();
+  String scannerRes = "Scan QR Code";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,15 +18,36 @@ class _ScanQrCodeState extends State<ScanQrCode> {
         title: const Text("Scan Qr Code"),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: urlController,
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(14.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(scannerRes),
+              const SizedBox(height: 4),
+              const Divider(),
+              const SizedBox(height: 4),
+              MyButton(
+                title: "Scan QR Code",
+                onPressed: scanQRCode,
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  void scanQRCode() async {
+    try {
+      scannerRes = await FlutterBarcodeScanner.scanBarcode(
+          "#000000", "Cancel", true, ScanMode.QR);
+      setState(() {});
+    } catch (e) {
+      // ignore: use_build_context_synchronously
+      ScaffoldMessenger.of(context)
+        ..hideCurrentSnackBar()
+        ..showSnackBar(SnackBar(content: Text(e.toString())));
+    }
   }
 }
